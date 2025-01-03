8113593ccddb6d20b47ef67ddc64d3bc79bcace3
Fnafke <elin.bachvarov@student.ucll.be>
2025-01-03 15:30:36 +0100

    basic login page component made in the frontend

---
+++ front-end/Components/login/LoginForm.tsx
@@ -0,0 +1,14 @@
+const LoginForm: React.FC = () => {
+    return (
+        <>
+        <form className="flex flex-col justify-center">
+            <label>Email</label>
+            <input></input>
+            <label>Password</label>
+            <input></input>
+        </form>
+        </>
+    );
+}
+
+export default LoginForm;
---
+++ front-end/pages/login/index.tsx
@@ -0,0 +1,13 @@
+import Header from "@/Components/Header";
+import LoginForm from "@/Components/login/LoginForm";
+
+const Login: React.FC = () => {
+    return (
+        <>
+        <Header/>
+        <LoginForm/>
+        </>
+    );
+}
+
+export default Login;
