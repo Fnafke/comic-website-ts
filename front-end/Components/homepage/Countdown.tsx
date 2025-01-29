f066383715876eb2ba71868c04b68f9414752857
Fnafke <elin.bachvarov@student.ucll.be>
2025-01-29 19:26:03 +0100

    added a countdown timer

---
+++ front-end/Components/homepage/Countdown.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import useInterval from "use-interval";
+
+type Props = {
+    date: Date,
+}
+
+
+const Countdown: React.FC<Props> = ({date}: Props) => {
+    const [currenttime, setCurrentTime] = useState<Date>(new Date());
+    const [countdown, setCountdown] = useState<{days: string | number, hours: string | number, minutes: string | number, seconds: string | number}>({days: 0, hours: 0, minutes: 0, seconds: 0});
+
+    const calculateCountdown = () => {
+        const calculated_time = date.getTime() - currenttime.getTime();
+        
+        const days = Math.floor(calculated_time / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((calculated_time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((calculated_time % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((calculated_time % (1000 * 60)) / 1000);
+
+        const formatNumber = (num: number) => num < 10 ? `0${num}` : num;
+
+        setCountdown({
+            days: formatNumber(days),
+            hours: formatNumber(hours),
+            minutes: formatNumber(minutes),
+            seconds: formatNumber(seconds)
+        });
+    }
+
+    useInterval(() => {
+        setCurrentTime(new Date());
+    }, 1000)
+
+    useEffect(() => {
+        calculateCountdown();
+    }, [currenttime]);
+
+    return <>
+        <div className="flex pl-52">
+            <p>{countdown.days}:{countdown.hours}:{countdown.minutes}:{countdown.seconds}</p>
+        </div>
+    </>
+}
+
+export default Countdown;
