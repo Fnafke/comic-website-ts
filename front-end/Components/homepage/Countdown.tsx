c8ca88b5b94c653ecc1e80352ae682bf6a2222fd
Fnafke <elin.bachvarov@student.ucll.be>
2025-01-31 17:47:06 +0100

    Countdown function updated

--- front-end/Components/homepage/Countdown.tsx
+++ front-end/Components/homepage/Countdown.tsx
@@ -9,7 +9,8 @@ type Props = {
 const Countdown: React.FC<Props> = ({date}: Props) => {
     const [currenttime, setCurrentTime] = useState<Date>(new Date());
     const [countdown, setCountdown] = useState<{days: string | number, hours: string | number, minutes: string | number, seconds: string | number}>({days: 0, hours: 0, minutes: 0, seconds: 0});
-
+    const [statusMessage, setStatusMessage] = useState<string>('');
+    
     const calculateCountdown = () => {
         const calculated_time = date.getTime() - currenttime.getTime();
         
@@ -17,6 +18,11 @@ const Countdown: React.FC<Props> = ({date}: Props) => {
         const hours = Math.floor((calculated_time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((calculated_time % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((calculated_time % (1000 * 60)) / 1000);
+
+        if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+            setStatusMessage("New Chapter has been released!")
+            return;
+        }
 
         const formatNumber = (num: number) => num < 10 ? `0${num}` : num;
 
@@ -37,8 +43,8 @@ const Countdown: React.FC<Props> = ({date}: Props) => {
     }, [currenttime]);
 
     return <>
-        <div className="flex pl-52 text-white">
-            <p>{countdown.days}:{countdown.hours}:{countdown.minutes}:{countdown.seconds}</p>
+        <div className="text-white text-center font-noto-serif-jp text-6xl font-bold">
+            {statusMessage? statusMessage : <p>{countdown.days}:{countdown.hours}:{countdown.minutes}:{countdown.seconds}</p>}
         </div>
     </>
 }
