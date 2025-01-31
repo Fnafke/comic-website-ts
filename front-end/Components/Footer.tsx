1a6c7f25bca7e358677195da0fd634a596480195
Fnafke <elin.bachvarov@student.ucll.be>
2025-01-31 17:21:54 +0100

    added a footer

---
+++ front-end/Components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+        <>
+        <footer className="text-white text-center">©Fnafke {new Date().getFullYear()}</footer>
+        </>
+    )
+}
+
+export default Footer
