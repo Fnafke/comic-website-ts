6022e24889efa0ea10529b12295468a7afe8cc6c
Fnafke <elinbachvarov@gmail.com>
2025-02-07 21:29:27 +0100

    fixed up the routes get requests to not include a req body

--- back-end/controller/chapter.routes.ts
+++ back-end/controller/chapter.routes.ts
@@ -5,18 +5,18 @@ const chapterRouter = express.Router()
 
 
 // GET REQUESTS
-chapterRouter.get('/', async(req: Request, res: Response, next: NextFunction) => {
+chapterRouter.get('/:chapterType', async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const chapters = await chapterService.getAllChapters(req.body.chapterType);
+        const chapters = await chapterService.getAllChapters(req.params.chapterType);
         res.status(200).json(chapters);
     } catch (error: any) {
         res.status(400).json({status: 'Error', errorMessage: error.message})
     }
 })
 
-chapterRouter.get('/:chapterNumber', async(req: Request, res: Response, next: NextFunction) => {
+chapterRouter.get('/:chapterNumber/:chapterType', async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const chapter = await chapterService.getChapter(parseInt(req.params.chapterNumber), req.body.chapterType);
+        const chapter = await chapterService.getChapter(parseInt(req.params.chapterNumber), req.params.chapterType);
         res.status(200).json(chapter);
     } catch (error: any) {
         res.status(400).json({status: 'Error', errorMessage: error.message})
