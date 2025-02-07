0fb8c85720a58451056ec2160866b411673ca794
Fnafke <elinbachvarov@gmail.com>
2025-02-07 21:28:29 +0100

    fixed access for all things relating to /chapters

--- back-end/app.ts
+++ back-end/app.ts
@@ -19,7 +19,7 @@ app.use(
         secret: process.env.JWT_SECRET || 'default_secret',
         algorithms: ['HS256'],
     }).unless({
-        path: ["/api-docs", /^\/api-docs\/.*/, "/users/login", "/users/signup", '/chapters', "/status"],
+        path: ["/api-docs", /^\/api-docs\/.*/, "/users/login", "/users/signup", "/status", /^\/chapters\/.*/],
     })
 )
 
