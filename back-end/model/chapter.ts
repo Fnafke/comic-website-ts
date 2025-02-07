ffa35a857de9e33eb0f54263d435ab42ef06767f
Fnafke <elinbachvarov@gmail.com>
2025-02-07 20:13:52 +0100

    Fixed the model, forgot to change the parameter names

--- back-end/model/chapter.ts
+++ back-end/model/chapter.ts
@@ -2,20 +2,20 @@ import { Chapter as ChapterPrisma } from "@prisma/client";
 
 export class Chapter {
     readonly id?: number;
-    readonly chapter_number: number;
-    readonly chapter_description: string;
-    readonly chapter_images_hash: string;
+    readonly chapterNumber: number;
+    readonly chapterDescription: string;
+    readonly chapterImagesHash: string;
 
-    constructor(chapter: {id?: number, chapter_number: number, chapter_description: string, chapter_images_hash: string}) {
+    constructor(chapter: {id?: number, chapterNumber: number, chapterDescription: string, chapterImagesHash: string}) {
         this.id = chapter.id;
-        this.chapter_number = chapter.chapter_number;
-        this.chapter_description = chapter.chapter_description;
-        this.chapter_images_hash = chapter.chapter_images_hash;
+        this.chapterNumber = chapter.chapterNumber;
+        this.chapterDescription = chapter.chapterDescription;
+        this.chapterImagesHash = chapter.chapterImagesHash;
     }
 
-    static from({id, chapter_number, chapter_description, chapter_images_hash}: ChapterPrisma) {
+    static from({id, chapterNumber, chapterDescription, chapterImagesHash}: ChapterPrisma) {
         return new Chapter(
-            {id, chapter_number, chapter_description, chapter_images_hash}
+            {id, chapterNumber, chapterDescription, chapterImagesHash}
         )
     }
 }
