e81074518365238f5ef5aaec9c5776765863712d
Fnafke <elinbachvarov@gmail.com>
2025-02-08 16:18:31 +0100

    finished the draftChapter page? Only thing needs to be done is the singleChapterOverview component

--- front-end/pages/draftChapters/[Draft]/[draftChapter].tsx
+++ front-end/pages/draftChapters/[Draft]/[draftChapter].tsx
@@ -1,11 +1,18 @@
+import SingleChapterOverview from "@/Components/draftChapters/SingleChapterOverview";
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
+import { useRouter } from "next/router";
 
 const draftChapter: React.FC = () => {
+    const router = useRouter();
+
+    const {chapterType, chapterNumber} = router.query
+
     return <>
         <title>Draft Chapters | Subvero</title>
         <Header/>
         <h1 className="text-center text-white text-4xl font-noto-serif-jp font-bold p-5">DRAFT CHAPTERS</h1>
+        <SingleChapterOverview chapterType={chapterType as string} chapterNumber={parseInt(chapterNumber as string)}/>
         <Footer/>
     </>
 }
