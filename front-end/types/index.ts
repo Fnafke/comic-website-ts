dde5cd95fd4a5cb96cd549e02da4480823c799db
Fnafke <elinbachvarov@gmail.com>
2025-02-09 14:17:31 +0100

    Created a DTO objects for the Imagur Response

--- front-end/types/index.ts
+++ front-end/types/index.ts
@@ -16,4 +16,42 @@ export type Chapter = {
     chapterType: string;
 }
 
+export type ImgurResponse = {
+    status: number;
+    success: boolean;
+    data: ImgurImage[];
+}
+
+export type ImgurImage = {
+    id: string;
+    deletehash: string;
+    account_id: number;
+    account_url: string | null;
+    ad_type: number | null;
+    ad_url: string | null;
+    title: string | null;
+    description: string | null;
+    name: string;
+    type: string;
+    width: number;
+    height: number;
+    size: number;
+    views: number;
+    section: string | null;
+    vote: string | null;
+    bandwidth: number;
+    animated: boolean;
+    favorite: boolean;
+    in_gallery: boolean;
+    in_most_viral: boolean;
+    has_sound: boolean;
+    is_ad: boolean;
+    nsfw: boolean | null;
+    link: string;
+    tags: string[];
+    datetime: number;
+    mp4: string;
+    hls: string;
+}
+
 export type Role = 'Admin' | 'User';
