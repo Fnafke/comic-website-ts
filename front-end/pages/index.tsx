99a187334b6dddb2bc8ca02ed015115b0457f275
Fnafke <elinbachvarov@gmail.com>
2025-02-10 19:22:00 +0100

    Applied this fadeInSection compoment to the homepage

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,3 +1,4 @@
+import FadeInSection from "@/Components/FadeInSection";
 import Footer from "@/Components/Footer";
 import Header from "@/Components/Header";
 import Countdown from "@/Components/homepage/Countdown";
@@ -7,7 +8,10 @@ const Home = () => {
     <title>Introduction | Subvero</title>
     <Header/>
     <Countdown chapterNumber={1} date={new Date(2025, 7, 7, 0, 0, 0)}/>
+    <FadeInSection>
     <h1 className="pl-52 text-white text-7xl font-noto-serif-jp font-bold p-6">INTRODUCTION</h1>
+    </FadeInSection>
+    <FadeInSection>
     <p className="pl-52 text-3xl font-noto-serif-jp w-5/6 break-words text-white tracking-wide">
     I originally started writing and drawing this comic as a way to procrastinate during exam season. 
     However, over time, it has grown into something I genuinely enjoy doing in my free time. Subvero is a story of tragedy, revenge, and hatred. 
@@ -17,10 +21,15 @@ const Home = () => {
     The story has undergone significant changes over the years in the Draft Chapters and is now in need of a rewrite. 
     The rewritten chapters will be available separately, as the draft version only represents about 40% of the full story.
     </p>
+    </FadeInSection>
+    <FadeInSection>
     <h1 className="pl-52 text-white text-7xl font-noto-serif-jp font-bold pt-16">CHARACTERS</h1>
+    </FadeInSection>
+    <FadeInSection>
     <p className="pl-52 text-3xl font-noto-serif-jp w-5/6 break-words text-white tracking-wide p-6">As the story progresses, the character page will be regularly updated with the latest information on each character. 
      These updates can range from small additions, such as new abilities, to major revelations like full backstories. 
      Something to look forward to!</p>
+     </FadeInSection>
     <Footer/>
   </>
 }
