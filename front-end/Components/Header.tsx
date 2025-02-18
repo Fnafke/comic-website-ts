e3165816cd8e1cdb79ad1d7a7deb803d8a52d75f
Fnafke <elinbachvarov@gmail.com>
2025-02-18 23:10:56 +0100

    Some effort at a responsive design

--- front-end/Components/Header.tsx
+++ front-end/Components/Header.tsx
@@ -20,30 +20,31 @@ const Header: React.FC = () => {
 
     return (
         <div className="flex z-50 fixed">
-          <button
-            className="bg-black h-screen w-20 flex items-start justify-center fixed z-50 outline outline-1 outline-white"
+            <button
+            className="bg-black h-screen w-20 flex items-start justify-center fixed z-50 outline outline-1 outline-white 
+                      max-lg:left-2.5 max-lg:top-3/4 max-lg:w-10 max-lg:h-10"
             onClick={handleOpen}
           >
-          <div className="pt-9 space-y-1">
-            <span
-              className={`block h-1 w-10 bg-white transition-transform duration-300 ${
-                isOpen ? 'rotate-45 translate-y-2' : ''
-              }`}
-            ></span>
-            <span
-              className={`block h-1 w-10 bg-white transition-opacity duration-300 ${
-                isOpen ? 'opacity-0' : ''
-              }`}
-            ></span>
-            <span
-              className={`block h-1 w-10 bg-white transition-transform duration-300 ${
-                isOpen ? '-rotate-45 -translate-y-2' : ''
-              }`}
-            ></span>
-          </div>
+            <div className="pt-9 space-y-1 max-lg:pt-2.5">
+              <span
+                className={`block h-1 w-10 bg-white transition-transform duration-300 max-lg:w-5 ${
+                  isOpen ? 'rotate-45 translate-y-2' : ''
+                }`}
+              ></span>
+              <span
+                className={`block h-1 w-10 bg-white transition-opacity duration-300 max-lg:w-5 ${
+                  isOpen ? 'opacity-0' : ''
+                }`}
+              ></span>
+              <span
+                className={`block h-1 w-10 bg-white transition-transform duration-300  max-lg:w-5 ${
+                  isOpen ? '-rotate-45 -translate-y-2' : ''
+                }`}
+              ></span>
+            </div>
           </button>
           <div
-            className={`fixed inset-0  bg-black text-4xl flex justify-center items-center gap-12 transition-all duration-500 ${
+            className={`fixed inset-0  bg-black text-4xl flex justify-center items-center gap-12 transition-all duration-500 max-lg:flex-col max-lg:text-4xl ${
               isOpen ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-full'
             }`}
           >
--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -9,10 +9,12 @@ const Home = () => {
     <Header/>
     <Countdown chapterNumber={1} date={new Date(2025, 7, 7, 0, 0, 0)}/>
     <FadeInSection>
-    <h1 className="pl-52 text-white text-7xl font-noto-serif-jp font-bold p-6">INTRODUCTION</h1>
+    <h1 className="pl-52 text-white text-7xl font-noto-serif-jp font-bold p-6 
+    max-lg:pl-0 max-lg:text-4xl max-lg:text-center max-lg:m-auto">INTRODUCTION</h1>
     </FadeInSection>
     <FadeInSection>
-    <p className="pl-52 text-3xl font-noto-serif-jp w-5/6 break-words text-white tracking-wide">
+    <p className="pl-52 text-3xl font-noto-serif-jp w-5/6 break-words text-white tracking-wide 
+    max-lg:pl-0 max-lg:text-xl max-lg:text-center max-lg:m-auto">
     I originally started writing and drawing this comic as a way to procrastinate during exam season. 
     However, over time, it has grown into something I genuinely enjoy doing in my free time. Subvero is a story of tragedy, revenge, and hatred. 
     The protagonist, Yuta Tokito (originally referred to as Jotaro Kujo in the Draft Chapters), lost his family at a young age 
@@ -23,10 +25,12 @@ const Home = () => {
     </p>
     </FadeInSection>
     <FadeInSection>
-    <h1 className="pl-52 text-white text-7xl font-noto-serif-jp font-bold pt-16">CHARACTERS</h1>
+    <h1 className="pl-52 text-white text-7xl font-noto-serif-jp font-bold pt-16 
+    max-lg:pl-0 max-lg:text-4xl max-lg:text-center max-lg:m-auto">CHARACTERS</h1>
     </FadeInSection>
     <FadeInSection>
-    <p className="pl-52 text-3xl font-noto-serif-jp w-5/6 break-words text-white tracking-wide p-6">As the story progresses, the character page will be regularly updated with the latest information on each character. 
+    <p className="pl-52 text-3xl font-noto-serif-jp w-5/6 break-words text-white tracking-wide p-6 
+    max-lg:pl-0 max-lg:text-xl max-lg:text-center max-lg:m-auto">As the story progresses, the character page will be regularly updated with the latest information on each character. 
      These updates can range from small additions, such as new abilities, to major revelations like full backstories. 
      Something to look forward to!</p>
      </FadeInSection>
