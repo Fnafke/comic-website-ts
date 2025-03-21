0d0ec5dfdf27aa3cb77486226728c2c1ae9cc45c
Fnafke <elinbachvarov@gmail.com>
2025-03-21 15:53:42 +0100

    added support for seconds, minutes and hours ago

--- front-end/Components/draftChapters/DateConverter.tsx
+++ front-end/Components/draftChapters/DateConverter.tsx
@@ -6,8 +6,15 @@ const DateConverter: React.FC<Props> = ({date}: Props) => {
     const convertDate = (date: Date) => {
         date = new Date(date);
         const now = new Date();
-        const differenceInDays = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
 
+        const differenceInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+        if (differenceInSeconds < 60) return `${differenceInSeconds} seconds ago`;
+        if (differenceInSeconds < 3600) return `${Math.floor(differenceInSeconds / 60)} minutes ago`;
+        if (differenceInSeconds < 86400) return `${Math.floor(differenceInSeconds / 3600)} hours ago`;
+
+        const differenceInDays = Math.floor((now.getTime() - date.getTime()) / (1000 * 60 * 60 * 24));
+        
         if (differenceInDays === 0) return "Today";
         if (differenceInDays === 1) return "1 day ago";
         if (differenceInDays <= 6) return `${differenceInDays} days ago`;
