c3073b0ce6b79ae11487b26e8ceadb8517875bfc
Fnafke <elinbachvarov@gmail.com>
2025-04-13 13:31:25 +0200

    fixed the init script

--- back-end-java/src/main/resources/db/migration/V1__init.sql
+++ back-end-java/src/main/resources/db/migration/V1__init.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS chapters;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
@@ -10,12 +10,12 @@ CREATE TABLE users (
 );
 
 CREATE TABLE chapters (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY,
     chapter_cover_hash VARCHAR(10) NOT NULL,
     chapter_number INT NOT NULL,
     chapter_title VARCHAR(255) NOT NULL,
     chapter_description TEXT NOT NULL,
     chapter_images_hash VARCHAR(10) NOT NULL,
     chapter_type VARCHAR(8) NOT NULL,
-    chapter_release_date DATETIME NOT NULL
+    chapter_release_date TIMESTAMP NOT NULL
 );
